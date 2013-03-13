0d42558680f541388d33a72584f5f5b01ccdf04c
Davide Aversa <thek3nger@gmail.com>
2013-03-13 16:59:41 +0100

    First draft for a low-level actions layer
    No implementation. Only function signatures.

---
+++ Assets/Script/BotActions.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+/*!
+ * BotActions is a collection of low-level actions that a bot can do.
+ * 
+ * This actions can be invoked by an IBotBrain to perform high-level actions.
+ * 
+ * TODO: Complete Action Specification
+ */
+public class BotActions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	/*!
+	 * Move the bot to the grid location <i,j>.
+	 */
+	void MoveTo(int i, int j) {
+
+	}
+
+	/*!
+	 * Face in 'dir' direction.
+	 */
+	void FaceDirection(Vector3 dir) {
+
+	}
+
+	/*!
+	 * Attack
+	 */
+	void Attack() {
+
+	}
+
+	/*!
+	 * Grab the object in current location.
+	 */
+	void Grab() {
+
+	}
+
+	/*!
+	 * Release the object in current location.
+	 */
+	void Release() {
+
+	}
+
+	// TODO: Define more actions if needed!
+
+
+}
