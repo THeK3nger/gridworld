a61f3286cde24f5282fa4758356f78d64a0fb7ff
Davide Aversa <thek3nger@gmail.com>
2013-04-16 18:39:04 +0200

    New raster visibility mode in BotPerception.
    The goal of this mode is to avoid the raycasting visibility check
    used in the previous version.

--- Assets/Script/BotPerception.cs
+++ Assets/Script/BotPerception.cs
@@ -13,13 +13,18 @@ using System.Collections;
 [RequireComponent (typeof (Collider))]
 public class BotPerception : MonoBehaviour {
 
-	public bool raycastTest = true;
+	public bool raycastTest = false;
+    public bool rasterTest = true;
 
 	private BotControl parentControl;	/**< A reference to the IBotControl instance attache to the bot. */
+    private GridWorldMap mapworld;		/**< A reference to the original map. */
+
+    private string opaque = "@D";
 
 	// Use this for initialization
 	void Awake () {
 		parentControl = gameObject.transform.parent.gameObject.GetComponent<BotControl>();
+        mapworld = GameObject.Find("MapGenerator").GetComponent<GridWorldMap>();
 	}
 	
 	// Update is called once per frame
@@ -30,16 +35,28 @@ public class BotPerception : MonoBehaviour {
 	/**
 	 * Collision callback for entering objects.
 	 */
-	void OnTriggerEnter(Collider other) {
-		GameObject obj = other.gameObject;	// Reference to the entering object.
-		if (raycastTest) {
-			GameObject bot = gameObject.transform.parent.gameObject; // Reference to the bot object.
-            if (RayCastVisibility(obj,bot)) {
-				parentControl.objectEnteringFOV(obj);
-			}
-		} else {
-			parentControl.objectEnteringFOV(obj);
-		}
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;	// Reference to the entering object.
+        if (raycastTest)
+        {
+            GameObject bot = gameObject.transform.parent.gameObject; // Reference to the bot object.
+            if (RayCastVisibility(obj, bot))
+            {
+                parentControl.objectEnteringFOV(obj);
+            }
+            return;
+        }
+        if (rasterTest)
+        {
+            GameObject bot = gameObject.transform.parent.gameObject; // Reference to the bot object.
+            if (RasterVisibility(obj, bot))
+            {
+                parentControl.objectEnteringFOV(obj);
+            }
+            return;
+        }
+        parentControl.objectEnteringFOV(obj);
     }
 
 	/**
@@ -50,6 +67,13 @@ public class BotPerception : MonoBehaviour {
 		parentControl.objectLeavingFOV(obj);
     }
 
+    /**
+     * Check for raycast visibility.
+     * 
+     * \param obj The target object.
+     * \param bot The bot object.
+     * \return True if the object is visible from the bot. Flase otherwise.
+     */
     private bool RayCastVisibility(GameObject obj, GameObject bot)
     {
         RaycastHit hit = new RaycastHit();
@@ -58,6 +82,81 @@ public class BotPerception : MonoBehaviour {
         Vector3 direction = (obj.transform.position - (bot.transform.position + offset)).normalized;
         Physics.Raycast(bot.transform.position + offset, direction, out hit);
         return hit.transform.gameObject.Equals(obj);
+    }
+
+    /**
+     * Check for raster visibility.
+     * 
+     * This algorithm is a variation of a supercover algorithm based on 
+     * Bresenham rastering algorithm.
+     * 
+     * It use the global map to check if there are obstacles in the line
+     * between the bot and the object.
+     * 
+     * \param obj The target object.
+     * \param bot The bot object.
+     * \return True if the object is visible from the bot. Flase otherwise.
+     */
+    private bool RasterVisibility(GameObject obj, GameObject bot)
+    {
+        Vector3 botPos = bot.transform.position;
+        Vector3 objPos = obj.transform.position;
+        int[] botIJ = mapworld.GetIndexesFromWorld(botPos.x, botPos.z);
+        int[] objIJ = mapworld.GetIndexesFromWorld(objPos.x, objPos.z);
+        int y = botIJ[0];
+        int x = botIJ[1];
+        int dx = objIJ[1] - botIJ[1];
+        int dy = objIJ[0] - botIJ[0];
+        int xstep, ystep;
+        if (IsOpaque(y,x)) return false;
+        if (dy < 0) { ystep = -1; dy = -dy; } else ystep = 1;
+        if (dx < 0) { xstep = -1; dx = -dx; } else xstep = 1;
+        int ddx = 2 * dx;
+        int ddy = 2 * dy;
+        if (ddx >= ddy)
+        {
+            int errorprev = dx;
+            int error = dx;
+            for (int c = 0; c < dx-1; c++)
+            {
+                x += xstep;
+                error += ddy;
+                if (error > ddx)
+                {
+                    y += ystep;
+                    error -= ddx;
+                    if (error + errorprev <= ddx) { if (IsOpaque(y - ystep, x)) return false; }
+                    if (error + errorprev >= ddx) { if (IsOpaque(y, x - xstep)) return false; }
+                }
+                if (IsOpaque(y, x)) return false;
+                errorprev = error;
+            }
+        }
+        else
+        {
+            int errorprev = dy;
+            int error = dy;
+            for (int c = 0; c < dy-1; c++)
+            {
+                y += ystep;
+                error += ddx;
+                if (error > ddy)
+                {
+                    x += xstep;
+                    error -= ddy;
+                    if (error + errorprev >= ddy) { if (IsOpaque(y - ystep, x)) return false; }
+                    if (error + errorprev <= ddy) { if (IsOpaque(y, x - xstep)) return false; }
+                }
+                if (IsOpaque(y, x)) return false;
+                errorprev = error;
+            }
+        }
+        return true;
+    }
+
+    private bool IsOpaque(int i, int j)
+    {
+        return opaque.IndexOf(mapworld.GetMapElement(i, j)) != -1;
     }
 	
 }
