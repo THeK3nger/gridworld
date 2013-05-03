918d8f333ac69ce943e63165b4f2b66a1ffbfa76
Davide Aversa <thek3nger@gmail.com>
2013-05-03 12:52:41 +0200

    Now BotAction is "game indipendent".
    This is the first step for a complete decoupling between
    the Bot architecture and the game.
    
    The goal is to create a standard "framework" for NPC prototyping.

--- Assets/Script/Bot/BotActions.cs
+++ Assets/Script/Bot/BotActions.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
-
-using Pathfinding;
+using System;
+using System.Collections.Generic;
 
 /**
  * This class is a collection of low-level actions that a bot can do.
@@ -20,23 +20,35 @@ using Pathfinding;
  * \date 2013
  * \pre This class needs an instance of GridWorldMap and BotControl.
  */
+[RequireComponent(typeof(BotControl))]
 public class BotActions : GridWorldBehaviour {
 
-	public float moveBaseSpeed = 2; 		/**< Walk speed in m/s. */
-    public float speedDecreaseRate;         /**< Speed decrease rate by gold. */
+    private Dictionary<string,Action<string[]>> actions;
+    private Action abortAction;
 
 	private bool actionComplete = true;		/**< True if the last action is completed. */
 	private bool actionSuccess = true;		/**< True if the last action is completed successfully. */ 
 
 	private BotControl parentControl;		/**< A reference to a BotControl instance. */
-    private BotAttributes attributes;
+
 
 	// Use this for initialization
 	protected override void Awake () {
         base.Awake();
 		parentControl = gameObject.GetComponent<BotControl>();
-        attributes = gameObject.GetComponent<BotAttributes>();
+        actions = new Dictionary<string, Action<string[]>>();
 	}
+
+    public void RegisterNewAction(string command, Action<string[]> action)
+    {
+        Debug.Log("BotACTIONS: Registering " + command + " command");
+        actions[command] = action;
+    }
+
+    public void RegisterAbortAction(Action abortAction)
+    {
+        this.abortAction = abortAction;
+    }
 
 	/**
 	 * Perform the given action (if exists).
@@ -45,47 +57,23 @@ public class BotActions : GridWorldBehaviour {
 	 * \retval true If the action can be executed.
 	 * \retval false If the action can not be executed.
 	 */
-    public bool DoAction(string action)
+    public bool DoAction(string fullCommand)
     {
-        Debug.Log("Action Received: " + action);
-        string[] command = action.Split(' ');
-        if (action == "stop")
+        Debug.Log("Action Received: " + fullCommand);
+        string[] commands = fullCommand.Split(' ');
+        if (fullCommand == "stop")
         {
-            AbortCurrentAction();
+            abortAction.Invoke();
             return true;
         }
-        if (actionComplete)
+        if (actionComplete && actions.ContainsKey(commands[0]))
         {
+            Debug.Log("FUCK!");
             actionComplete = false;
             actionSuccess = false;
-            switch (command[0])
-            {
-                case "move":
-                    MoveTo(float.Parse(command[1]), float.Parse(command[2]));
-                    return true;
-                case "grab":
-                    Grab();
-                    return true;
-                case "drop" :
-                    Drop();
-                    return true;
-                default:
-                    return false;
-            }
+            actions[commands[0]].Invoke(commands);
         }
         return false;
-    }
-
-    public void AbortCurrentAction()
-    {
-        if (!actionComplete)
-        {
-            // If there are any animation running, stop it.
-            iTween.Stop(gameObject);
-            // Snap to the nearest grid point.
-            Vector3 current = gameObject.transform.position;
-            MoveTo(mapWorld.SnapCoord(current.x), mapWorld.SnapCoord(current.z));
-        }
     }
 
 	/*!
@@ -108,142 +96,19 @@ public class BotActions : GridWorldBehaviour {
 		return actionComplete && actionSuccess;
 	}
 
-	/**
-	 * Move the bot to the world plane <x,z> position.
-	 * 
-	 * \param x Desired x world location.
-	 * \param z Desired z world location.
-	 */
-	void MoveTo(float x, float z) {
-		Vector3 target = new Vector3 (x, 0, z);
-		(gameObject.GetComponent("Seeker") as Seeker).StartPath(gameObject.transform.position, target,this.PathFoundCallback);
-	}
-
-	/**
-	 * Calback called by Aron Pathfinding Algorithm when a path is available.
-	 * 
-	 * \param path The desired path.
-	 */
-	void PathFoundCallback(Path path) {
-        float moveSpeed = moveBaseSpeed * (float) System.Math.Exp(-speedDecreaseRate * attributes.goldCarrying);
-        moveSpeed = System.Math.Max(0.5f, moveSpeed);
-		animation.CrossFade("walk");
-		Vector3[] array_path = path.vectorPath.ToArray ();
-		iTween.MoveTo(gameObject, iTween.Hash
-		              (
-			"path", array_path,
-			"orienttopath", true,
-			"looktime", 1.0,
-			"lookahead", 0.05,
-			"axis", "y",
-			"y", 1,
-			"easetype", iTween.EaseType.linear,
-			"time", iTween.PathLength(array_path) / moveSpeed,
-            "oncomplete", "onMoveToPathComplete"
-			));
-	}
-
-	/**
-	 * Calback called by iTween when the path execution is completed.
-	 */
-	void onMoveToPathComplete() {
-		animation.CrossFade ("idle1");
-		actionComplete = true;
-		actionSuccess = true;
-		//LookAt(new Vector3(0,0,0));
-		parentControl.NotifyAction("move");
-	}
-
-	/**
-	 * Look at the `dir` world point.
-	 * 
-	 * \param dir The point that we want to look at.
-	 */
-	void LookAt(Vector3 dir) {
-		iTween.LookTo(gameObject,dir,1.0f);
-	}
-
-	/**
-	 * Attack
-	 */
-	void Attack() {
-
-	}
-
-	/**
-	 * Grab the object in current location.
-	 */
-    void Grab()
+    public void NotifyActionComplete()
     {
-        Vector3 current = gameObject.transform.position;
-        char currentItem = mapWorld.GetMapElement(current.x, current.z);
-        if (mapWorld.ElementIs("collectable", currentItem))
-        {
-            DestroyGameObjectByPosition(current, 'G');
-            attributes.goldCarrying += 100;
-            mapWorld.SetMapElement(current.x, current.z, '.');
-            parentControl.NotifyAction("grab");
-            actionSuccess = true;
-        }
-        else
-        {
-            Debug.Log("Nothing to Grab!!!");
-            actionSuccess = false;
-        }
-        actionComplete = true;
-        // TODO: How to invoke a return value?
-    }
-
-    /**
-     * Drop all the gold in the deposit.
-     */
-    void Drop()
-    {
-        Vector3 current = gameObject.transform.position;
-        if (mapWorld.GetArea(current.x, current.z) == attributes.spawnArea)
-        {
-            attributes.goldStored += attributes.goldCarrying;
-            attributes.goldCarrying = 0;
-            Debug.Log("SCORE: " + attributes.goldStored);
-            parentControl.NotifyAction("drop");
-            actionSuccess = true;
-        }
-        else
-        {
-            actionSuccess = false;
-        }
         actionComplete = true;
     }
 
-	/**
-	 * Release the object in current location.
-	 */
-	void Release() {
-
-	}
-
-    /**
-     * Destroy an object of a given type in the given position.
-     * 
-     * \param position The desired object position.
-     * \param type The desired object type.
-     */
-    private void DestroyGameObjectByPosition(Vector3 position, char type)
+    public void NotifyActionSuccess()
     {
-        Collider[] hitColliders = Physics.OverlapSphere(position, 1.5f);
-        foreach (Collider c in hitColliders)
-        {
-            SmartObjects so = c.gameObject.GetComponent<SmartObjects>();
-            if (so != null)
-            {
-                if (so.type[0] == type &&
-                    System.Math.Abs(c.gameObject.transform.position.x - position.x) < 0.1 &&
-                    System.Math.Abs(c.gameObject.transform.position.z - position.z) < 0.1)
-                {
-                    Destroy(c.gameObject);
-                    return;
-                }
-            }
-        }
+        actionSuccess = true;
     }
+
+    public void NotifyAction(string action)
+    {
+        parentControl.NotifyAction(action);
+    }
+
 }
---
+++ Assets/Script/Player/PlayerAction.cs
@@ -0,0 +1,192 @@
+using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+[RequireComponent(typeof(BotActions))]
+public class PlayerAction : GridWorldBehaviour
+{
+
+    private BotAttributes attributes;
+    private BotActions parentAction;
+
+    public float moveBaseSpeed = 2; 		/**< Walk speed in m/s. */
+    public float speedDecreaseRate;         /**< Speed decrease rate by gold. */
+
+	// Use this for initialization
+    protected override void Awake()
+    {
+        base.Awake();
+        attributes = gameObject.GetComponent<BotAttributes>();
+        parentAction = gameObject.GetComponent<BotActions>();
+	}
+
+    void Start()
+    {
+        parentAction.RegisterAbortAction(AbortCurrentAction);
+        parentAction.RegisterNewAction("move", MoveTo);
+        parentAction.RegisterNewAction("grab", Grab);
+        parentAction.RegisterNewAction("drop", Drop);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AbortCurrentAction()
+    {
+        if (!parentAction.LastActionComplete())
+        {
+            // If there are any animation running, stop it.
+            iTween.Stop(gameObject);
+            // Snap to the nearest grid point.
+            Vector3 current = gameObject.transform.position;
+            MoveTo(mapWorld.SnapCoord(current.x), mapWorld.SnapCoord(current.z));
+        }
+    }
+
+    /**
+     * Move the bot to the world plane <x,z> position.
+     * 
+     * \param x Desired x world location.
+     * \param z Desired z world location.
+     */
+    void MoveTo(float x, float z)
+    {
+        Vector3 target = new Vector3(x, 0, z);
+        (gameObject.GetComponent("Seeker") as Seeker).StartPath(gameObject.transform.position, target, this.PathFoundCallback);
+    }
+
+    /**
+     * Move the bot to the world plane <x,z> position.
+     * 
+     * \param x Desired x world location.
+     * \param z Desired z world location.
+     */
+    void MoveTo(string[] moveCommand)
+    {
+        Debug.Log("YEEEEEEEEEEEEEE");
+        float x = float.Parse(moveCommand[1]);
+        float z = float.Parse(moveCommand[2]);
+        MoveTo(x, z);
+    }
+
+    /**
+ * Calback called by Aron Pathfinding Algorithm when a path is available.
+ * 
+ * \param path The desired path.
+ */
+    void PathFoundCallback(Path path)
+    {
+        float moveSpeed = moveBaseSpeed * (float)System.Math.Exp(-speedDecreaseRate * attributes.goldCarrying);
+        moveSpeed = System.Math.Max(0.5f, moveSpeed);
+        animation.CrossFade("walk");
+        Vector3[] array_path = path.vectorPath.ToArray();
+        iTween.MoveTo(gameObject, iTween.Hash
+                      (
+            "path", array_path,
+            "orienttopath", true,
+            "looktime", 1.0,
+            "lookahead", 0.05,
+            "axis", "y",
+            "y", 1,
+            "easetype", iTween.EaseType.linear,
+            "time", iTween.PathLength(array_path) / moveSpeed,
+            "oncomplete", "onMoveToPathComplete"
+            ));
+    }
+
+    /**
+     * Calback called by iTween when the path execution is completed.
+     */
+    void onMoveToPathComplete()
+    {
+        animation.CrossFade("idle1");
+        parentAction.NotifyActionComplete();
+        parentAction.NotifyActionSuccess();
+        parentAction.NotifyAction("move");
+    }
+
+    /**
+     * Look at the `dir` world point.
+     * 
+     * \param dir The point that we want to look at.
+     */
+    void LookAt(Vector3 dir)
+    {
+        iTween.LookTo(gameObject, dir, 1.0f);
+    }
+
+    /**
+     * Attack
+     */
+    void Attack()
+    {
+
+    }
+
+    /**
+     * Grab the object in current location.
+     */
+    void Grab(string[] command)
+    {
+        Vector3 current = gameObject.transform.position;
+        char currentItem = mapWorld.GetMapElement(current.x, current.z);
+        if (mapWorld.ElementIs("collectable", currentItem))
+        {
+            DestroyGameObjectByPosition(current, 'G');
+            attributes.goldCarrying += 100;
+            mapWorld.SetMapElement(current.x, current.z, '.');
+            parentAction.NotifyAction("grab");
+            parentAction.NotifyActionSuccess();
+        }
+        else
+        {
+            Debug.Log("Nothing to Grab!!!");
+        }
+        parentAction.NotifyActionComplete();
+        // TODO: How to invoke a return value?
+    }
+
+    /**
+     * Drop all the gold in the deposit.
+     */
+    void Drop(string[] command)
+    {
+        Vector3 current = gameObject.transform.position;
+        if (mapWorld.GetArea(current.x, current.z) == attributes.spawnArea)
+        {
+            attributes.goldStored += attributes.goldCarrying;
+            attributes.goldCarrying = 0;
+            Debug.Log("SCORE: " + attributes.goldStored);
+            parentAction.NotifyAction("drop");
+            parentAction.NotifyActionSuccess();
+        }
+        parentAction.NotifyActionComplete();
+    }
+
+    /**
+     * Destroy an object of a given type in the given position.
+     * 
+     * \param position The desired object position.
+     * \param type The desired object type.
+     */
+    private void DestroyGameObjectByPosition(Vector3 position, char type)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(position, 1.5f);
+        foreach (Collider c in hitColliders)
+        {
+            SmartObjects so = c.gameObject.GetComponent<SmartObjects>();
+            if (so != null)
+            {
+                if (so.type[0] == type &&
+                    System.Math.Abs(c.gameObject.transform.position.x - position.x) < 0.1 &&
+                    System.Math.Abs(c.gameObject.transform.position.z - position.z) < 0.1)
+                {
+                    Destroy(c.gameObject);
+                    return;
+                }
+            }
+        }
+    }
+}
